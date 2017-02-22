6aa4bed453adf1911c289382e1afc36b3e413952
Nick <nickheyek@protonmail.com>
2017-02-22 11:48:57 -0600

    add gitignore, improve functionality of trait data retrievel

--- experiment-trait-data-visualizer/trait_data_retrieval.py
+++ experiment-trait-data-visualizer/trait_data_retrieval.py
@@ -4,13 +4,26 @@ def get_trait_data(year, start_month, end_month):
 
 	all_traits = get_trait_records(year, start_month, end_month)
 
-	variable_count_dict = {}
+	variable_data = {}
 
 	for trait in all_traits:
 
-		if trait["trait"]["variable_id"] not in variable_count_dict:
-			variable_count_dict[trait["trait"]["variable_id"]] = 1
-			
-		variable_count_dict[trait["trait"]["variable_id"]] += 1
+		trait_variable_id = trait["trait"]["variable_id"]
+		trait_date = trait["trait"]["date"]
 
-	return variable_count_dict
+		if trait_variable_id not in variable_data:
+			variable_data[ trait_variable_id ] = {}
+			variable_data[ trait_variable_id ]["count"] = 1
+			variable_data[ trait_variable_id ]["dates"] = {}
+			variable_data[ trait_variable_id ]["dates"][trait_date] = 1
+		else:
+			variable_data[ trait["trait"]["variable_id"]]["count"]  += 1
+
+			if trait_date not in variable_data[ trait_variable_id ]["dates"]:
+				variable_data[ trait_variable_id ]["dates"][trait_date] = 1
+			else:
+				variable_data[ trait_variable_id ]["dates"][trait_date] += 1
+
+	return variable_data
+
+print(get_trait_data(2016, 8, 12))
