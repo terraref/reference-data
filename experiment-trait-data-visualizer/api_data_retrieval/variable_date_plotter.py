1ddf95a3bedabafef8e36b4919d281cc4d2dd0c4
Nick <nickheyek@protonmail.com>
2017-03-13 11:39:08 -0500

    html embed, box chart functionality, clean up styling

--- experiment-trait-data-visualizer/api_data_retrieval/variable_date_plotter.py
+++ experiment-trait-data-visualizer/api_data_retrieval/variable_date_plotter.py
@@ -1,6 +1,13 @@
 from .trait_data_retrieval import get_trait_data
 import pygal
-from pygal.style import CleanStyle
+from pygal.style import Style 
+
+same_color_style = Style(background='transparent',
+							colors=('#000000','#000000'),
+							title_font_size=24,
+							value_font_size=20,
+							major_label_font_size=16,
+							label_font_size=16)
 
 def plot_dates(year, start_month, end_month):
 
@@ -10,26 +17,33 @@ def plot_dates(year, start_month, end_month):
 
 	for trait in traits:
 
-		means = []
 		date_labels = []
 		date_dict = trait_data[trait]["dates"]
+		variable_name = trait_data[trait]["variable_data"]["name"]
+		variable_standard_name = trait_data[trait]["variable_data"]["standard_name"]
+		units = trait_data[trait]["variable_data"]["units"]
+
+		if variable_standard_name:
+			chart_title = variable_standard_name
+		else:
+			chart_title = variable_name
+
+		chart_title += '\n' + str(units)
 
 		for date in date_dict:
 			date_labels.append(date)
 		date_labels.sort()
 
-		chart = pygal.XY(show_legend=False, xrange=(0, len(date_labels)), style=CleanStyle)
+		
+		chart = pygal.Box(show_legend=False, style=same_color_style,
+			title=chart_title, show_y_guides=False)
 
 		for date in range(0, len(date_labels)):
 			date_means = []
 			for mean in date_dict[date_labels[date]]["means"]:
-				date_means.append(mean)
-			means.append(date_means)
+				date_means.append({'value':mean, 'color':'#1a6d2f'})
 
-
-		for date in range(0, len(date_labels)):
-			chart.add(date_labels[date], [(date, mean) for mean in means[date]])
+			chart.add('', date_means)
 
 		chart.x_labels = date_labels
-
 		chart.render_to_file('static/charts/chart_' + str(trait) + '.svg')
