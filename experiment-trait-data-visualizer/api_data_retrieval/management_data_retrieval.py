df7c68c55b7c6ccae8332a604c8e77a9272d50c8
Nick Heyek <nickheyek@protonmail.com>
2017-04-05 15:40:01 -0500

    timeline improvements

--- experiment-trait-data-visualizer/api_data_retrieval/management_data_retrieval.py
+++ experiment-trait-data-visualizer/api_data_retrieval/management_data_retrieval.py
@@ -4,7 +4,7 @@ def get_management_data(year, start_month, end_month):
 
 	all_managements = get_management_records(2016, 8, 12)
 
-	managements_data = []
+	managements_data = {}
 
 	prev_day = 0
 	prev_month = 0
@@ -12,26 +12,28 @@ def get_management_data(year, start_month, end_month):
 
 	for management in all_managements:
 
-		management_data_entry = {}
-		management_data_entry["date"] = management["management"]["date"]
-		management_data_entry["type"] = management["management"]["mgmttype"]
-		management_data_entry["notes"] = management["management"]["notes"]
-
-		timeline_dist = 0
-		date_arr = management_data_entry["date"].split('-')
+		management_date = management["management"]["date"]
+		date_arr = management_date.split('-')
 		curr_year = int(date_arr[0])
 		curr_month = int(date_arr[1])
 		curr_day = int(date_arr[2])
 
-		if (prev_year != 0):
-			timeline_dist += 365 * (curr_year - prev_year) + 30 * (curr_month - prev_month) + (curr_day - prev_day)
+		management_data_entry = {}
+		management_data_entry["type"] = management["management"]["mgmttype"]
+		management_data_entry["notes"] = management["management"]["notes"]
 
-		management_data_entry["timeline_dist"] = timeline_dist
+		if management_date not in managements_data:
+			managements_data[management_date] = {}
+			managements_data[management_date]["entries"] = []
+			timeline_dist = 0
+			if (prev_year != 0):
+				timeline_dist += 365 * (curr_year - prev_year) + 30 * (curr_month - prev_month) + (curr_day - prev_day)
+			managements_data[management_date]["timeline_dist"] = timeline_dist
+
+		managements_data[management_date]["entries"].append(management_data_entry)
 
 		prev_year = curr_year
 		prev_month = curr_month
 		prev_day = curr_day
-
-		managements_data.append(management_data_entry)
 
 	return managements_data
