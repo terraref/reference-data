22fde4b63f893c2273aef0e657fc09aa7bf28ce9
Nick Heyek <nickheyek@protonmail.com>
2017-06-13 12:08:51 -0500

    add basic input/output

--- experiment-trait-data-visualizer/app.R
+++ experiment-trait-data-visualizer/app.R
@@ -1,8 +1,20 @@
 library(shiny)
 
 ui <- fluidPage(
-  titlePanel("BETYdb Trait Data")
+  titlePanel("BETYdb Trait Data"),
+  sidebarLayout (
+    sidebarPanel(
+      selectInput('selectedSeason', 'Season', c('Season 1', 'Season 2', 'Season 3')),
+      selectInput('selectedVariable','Variable', c('Variable 1', 'Variable 2', 'Variable 3'))
+    ),
+    mainPanel(
+      plotOutput('traitPlot')
+    )
+  )
 )
 
-server <- function(input, output) {}
+server <- function(input, output) {
+  output$traitPlot <- renderPlot({
+  })
+}
 shinyApp(ui=ui, server=server)
