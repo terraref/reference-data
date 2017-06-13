1d71d3ab3a605ad7d7012005486300afc438b9df
Nick Heyek <nickheyek@protonmail.com>
2017-06-13 12:32:32 -0500

    add experiments to input panel

--- experiment-trait-data-visualizer/app.R
+++ experiment-trait-data-visualizer/app.R
@@ -1,11 +1,17 @@
 library(shiny)
+library(traits)
+
+options(betydb_key = readLines('~/.betykey', warn = FALSE),
+        betydb_url = "https://terraref.ncsa.illinois.edu/bety/",
+        betydb_api_version = 'beta')
+experiments <- betydb_query(table='experiments')
 
 ui <- fluidPage(
   titlePanel("BETYdb Trait Data"),
   sidebarLayout (
     sidebarPanel(
-      selectInput('selectedSeason', 'Season', c('Season 1', 'Season 2', 'Season 3')),
-      selectInput('selectedVariable','Variable', c('Variable 1', 'Variable 2', 'Variable 3'))
+      selectInput('selectedExp', 'Experiment', experiments[,'name']),
+      selectInput('selectedVariable','Variable', c('Variable 1', 'Variable 2'))
     ),
     mainPanel(
       plotOutput('traitPlot')
