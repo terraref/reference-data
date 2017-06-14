5f4b4b37df862e316f8bd343ffa57a1ac4c2df23
Nick Heyek <nickheyek@protonmail.com>
2017-06-14 11:45:24 -0500

    add select variable functionality

--- experiment-trait-data-visualizer/app.R
+++ experiment-trait-data-visualizer/app.R
@@ -1,17 +1,22 @@
 library(shiny)
 library(traits)
+knitr::opts_chunk$set(echo = FALSE, cache = TRUE)
 
 options(betydb_key = readLines('~/.betykey', warn = FALSE),
         betydb_url = "https://terraref.ncsa.illinois.edu/bety/",
         betydb_api_version = 'beta')
+
 experiments <- betydb_query(table='experiments')
+
+traitData <- betydb_query(table='traits', date='~2016-08')
+varsIdsObserved <- as.numeric(unique(traitData[,'variable_id']))
 
 ui <- fluidPage(
   titlePanel("BETYdb Trait Data"),
   sidebarLayout (
     sidebarPanel(
       selectInput('selectedExp', 'Experiment', experiments[,'name']),
-      selectInput('selectedVariable','Variable', c('Variable 1', 'Variable 2'))
+      selectInput('selectedVariable','Variable', varsObserved)
     ),
     mainPanel(
       plotOutput('traitPlot')
