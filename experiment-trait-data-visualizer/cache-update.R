49ab3ff33eca2b9e0518e06f3ea2ea3f783f7d6c
Nick Heyek <nickheyek@protonmail.com>
2017-07-12 14:33:34 -0500

    Add primitive functions for routine cache update

--- experiment-trait-data-visualizer/cache-update.R
+++ experiment-trait-data-visualizer/cache-update.R
@@ -37,11 +37,11 @@ refresh_cache_for_season <- function(season) {
     season_management_records <- rbind(season_management_records, curr_date_management_records[c('date', 'mgmttype')])
     
     curr_date_trait_records <- betydb_query(table = 'traits', date = paste0('~', curr_date), limit = 'none')
-    season_all_trait_records <- rbind(season_all_trait_records, curr_date_trait_records[c('variable_id', 'date', 'mean', 'cultivar_id')])
+    season_all_trait_records <- rbind(season_all_trait_records, curr_date_trait_records[c('id', 'variable_id', 'date', 'mean', 'cultivar_id')])
     
     curr_date = curr_date + days(1)
-    print(season_all_trait_records)
   }
+  
   season_management_records <- data.frame(season_management_records)
   season_all_trait_records <- data.frame(season_all_trait_records)
   
@@ -52,12 +52,12 @@ refresh_cache_for_season <- function(season) {
     
     curr_variable_cache_data <- list()
     
-    curr_variable_trait_records <- subset(season_all_trait_records, variable_id == curr_variable_id)[c('date', 'mean', 'cultivar_id')]
+    curr_variable_trait_records <- subset(season_all_trait_records, variable_id == curr_variable_id)[c('id', 'date', 'mean', 'cultivar_id')]
+    print(curr_variable_trait_records)
     curr_variable_metadata <- get_variable_metadata(curr_variable_id)
     
     curr_variable_cache_data[[ 'units' ]] <- curr_variable_metadata$units
     curr_variable_cache_data[[ 'traits' ]] <- curr_variable_trait_records
-    
     season_trait_data[[ curr_variable_metadata$name ]] <- curr_variable_cache_data
   }
   
@@ -66,7 +66,7 @@ refresh_cache_for_season <- function(season) {
   
   cache_data[[ toString(season['name']) ]] <- season_full_cache_data
   
-  save(cache_data, file="BETYdb-data.RData")
+  save(cache_data, file = "cache.RData")
 }
 
 refresh_cache <- function() {
@@ -79,7 +79,53 @@ refresh_cache <- function() {
   seasons <- cbind(seasons, name=experiment_names)
   
   apply(seasons, 1, refresh_cache_for_season)
+  
+  save(cache_data, file = "cache.RData")
+  Sys.setenv(last_cache_update_date = toString(Sys.Date()))
 }
 
-refresh_cache()
-save(cache_data, file="cache.RData")
+process_new_records(new_records) {
+  
+}
+
+process_updated_records() {
+  
+}
+
+update_cache <- function(last_update_date) {
+  
+  present_date <- Sys.Date()
+  
+  updated_records <- NULL
+  new_records <- NULL
+  
+  while (present_date - last_update_date != 0) {
+    
+    curr_date_new_records <- betydb_query(table = 'traits', date = paste0('~', last_update_date), limit = 'none')
+    curr_date_updated_records <- betydb_query(table = 'traits', updated_at = paste0('~', last_update_date), limit = 'none')
+    
+    new_records <- rbind(new_records, curr_date_new_records[c('id', 'date', 'mean', 'cultivar_id')])
+    updated_records <- rbind(updated_records, curr_date_new_records[c('id', 'date', 'mean', 'cultivar_id')])
+    
+    last_update_date <- last_update_date + days(1)
+  }
+  
+  if (!is.null(new_records)) {
+    # add new records to cache_data
+  }
+  if (!is.null(updated_records)) {
+    # update updated records in cache_data
+  }
+  
+  Sys.setenv(last_cache_update_date = toString(present_date))
+}
+
+last_update_date <- as.Date(Sys.getenv('last_cache_update_date'))
+
+if (is.null(last_update_date)) {
+  refresh_cache()
+} else {
+  load("cache.RData")
+  update_cache(last_update_date)
+}
+
